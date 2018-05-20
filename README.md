2bf36bea25c54933fe769deda787b1ab8a84acae
yunmap <kalmpink@korea.ac.kr>
2018-05-20 09:30:22 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Classrates
 Repository for Classrates Web site
+
+url : https://github.com/Microsoft/computerscience/blob/master/Labs/Web%20Development/Azure%20Web%20Apps%20and%20GitHub/Deploying%20a%20Cognitive%20Services%20Web%20Site%20to%20Azure%20via%20GitHub.md
--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "bc9b4d9e13b5421890cf241dbdc1a7cc";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
